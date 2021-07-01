bb7b3a4c655e0964e060102b8b8fcd4a00677855
Neeraj Dalal <admin@nrjdalal.com>
2021-07-01 23:01:18 +0530

    awesome hero section

--- components/index/Hero.jsx
+++ components/index/Hero.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <>
-      <div className="relative bg-sky-500">
+      <div className="relative bg-sky-200">
         {/* Divider */}
         <svg
           className="absolute bottom-0 h-3/4 w-full"
@@ -15,34 +15,38 @@ const Hero = () => {
           <path
             d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
             className="shape-fill"
-            fill="#38BDF8"
+            fill="#E0F2FE"
             fillOpacity="1"
           ></path>
         </svg>
         {/* Hero Section */}
-        <div className="relative z-[1] mod-container flex flex-col items-center text-center">
-          <div className="rounded-2xl w-36 h-36 sm:w-60 sm:h-60 mt-24 overflow-hidden">
+        <div className="relative mod-container flex flex-col md:flex-row md:items-center py-24">
+          {/* Image */}
+          <div className="rounded-2xl w-36 h-36 md:w-72 md:h-72 overflow-hidden">
             <Image
               src="/images/index/hero.webp"
               alt=""
-              height={500}
-              width={500}
+              height={576}
+              width={576}
               objectFit="cover"
             />
           </div>
-          <h1 className="font-semibold text-4xl sm:text-4xl mt-6 text-white">
-            Turing ideas to reality
-            <br />
-            via Design.
-          </h1>
-          <h2 className="text-xl sm:text-2xl mt-4 text-white">
-            At VeroXyle we love building great brands & digital experiences. Our
-            hardworking and dedicated team contribute to your project so that
-            you can achieve all the goals you&apos;ve set for yourself.
-          </h2>
-          <button className="bg-sky-700 text-white px-3 py-2 rounded-xl mt-8 mb-24 text-lg tracking-wider hover:bg-sky-900">
-            Contact Us
-          </button>
+          {/* h1 h2 button */}
+          <div className="flex-1 md:ml-8">
+            <h1 className="font-semibold text-4xl lg:text-4xl mt-6 md:mt-0 text-gray-900">
+              Turing ideas to reality
+              <br />
+              via Design.
+            </h1>
+            <h2 className="lg:text-xl mt-4 text-gray-600">
+              At VeroXyle we love building great brands & digital experiences.
+              Our hardworking and dedicated team contribute to your project so
+              that you can achieve all the goals you&apos;ve set for yourself.
+            </h2>
+            <button className="bg-sky-500 text-white w-72 py-2.5 rounded-xl mt-8 text-lg tracking-wider hover:bg-sky-900">
+              Contact Us
+            </button>
+          </div>
         </div>
       </div>
     </>
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,7 @@ import Hero from '../components/index/Hero'
 const index = () => {
   return (
     <>
-      <div className="bg-sky-400 h-screen w-screen">
-        <Hero />
-      </div>
+      <Hero />
     </>
   )
 }
