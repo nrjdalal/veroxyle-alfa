d597b593e66cb1fbe50886f43179b0edb673c446
Neeraj Dalal <admin@nrjdalal.com>
2021-07-01 17:01:57 +0530

    fixed notice bar

--- components/Notice.jsx
+++ components/Notice.jsx
@@ -7,7 +7,7 @@ const Notice = () => {
         <div className="mod-container py-1.5 flex text-white items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-6 w-6 hidden sm:block"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -19,10 +19,9 @@ const Notice = () => {
               d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
+          <span className="hidden sm:block">&nbsp;</span>
           <p>
-            <span className="font-semibold">
-              &nbsp;We&apos;re hiring!&nbsp;
-            </span>
+            <span className="font-semibold">We&apos;re hiring!</span>&nbsp;
             <Link href="/careers">
               <a className="underline">Browse Careers</a>
             </Link>
